a99110358006f8978ff5d22ba06a3c931fedbdee
jcch96 <joelchanch@gmail.com>
2021-03-25 18:19:36 +0800

    l_plan.cpp working

--- local_planner/src/l_plan.cpp
+++ local_planner/src/l_plan.cpp
@@ -32,13 +32,17 @@ class Robot
 
 		// Map info
 		double len_x, len_y, res;
-		int left_bound_x, right_bound_x;
-		std::array<int,2> vert_bound, forward_bound_x;
+		int left_bound, right_bound;
+		std::array<int,2> vert_bound, forward_bound;
+
+		std::vector<std::vector<int>> map_mat;
+
+		local_planner::CmapClear bools;
 
 	public:
 		Robot(ros::NodeHandle *nh)
 		{	
-			CostMap = nh->subscribe("/map", 1000, &Robot::mapCallback, this);
+			CostMap = nh->subscribe("/semantics/costmap_generator/occupancy_grid", 1000, &Robot::mapCallback, this);
 			CmdVelPub = nh->advertise<geometry_msgs::Twist>("panthera_cmd",100);
 			cmap_clear = nh->advertise<local_planner::CmapClear>("check_cmap",100);
 
@@ -52,20 +56,21 @@ class Robot
 			len_y = msg.info.height;
 			res = msg.info.resolution;
 			auto data_pts = msg.data;
-
-			double** map_mat;
-
+			fpCoordinates();
 			int n = 0;
 			for (int i=0; i<(int)len_x; i++)
 			{	
+				std::vector<int> v = {};
 				for (int j=0; j<(int)len_y; j++)
 				{	
-					map_mat[j][i] = data_pts[n];
+					v.push_back(data_pts[n]);
 					n++;
 				}
+				map_mat.push_back(v);
+				//std::cout << n << std::endl;
 			}
-
-			checkclear(map_mat);
+			std::cout << "checking clear" << std::endl;
+			checkclear();
 
 		}
 
@@ -75,43 +80,47 @@ class Robot
 			centre[0][0] = len_x/2;
 			centre[0][1] = len_y/2;
 
-			float horz_dist = width/2;
-			float vert_dist = length/2;
+			float horz_dist = width/2; // x axis
+			float vert_dist = length/2; // y axis
 
 			int horz_pix = (int)ceil(horz_dist/res);
 			int vert_pix = (int)ceil(vert_dist/res);
 
-			left_bound_x = (int)round(len_x/2) - horz_pix;
-			right_bound_x = (int)ceil(len_x/2) - horz_pix;
-			vert_bound = {(int)round(len_y/2) - vert_pix, (int)ceil(len_y/2) + vert_pix};
+			left_bound = (int)ceil(len_x/2) - vert_pix;
+			right_bound = (int)round(len_x/2) + vert_pix;
+			vert_bound = {(int)round(len_x/2) - vert_pix, (int)ceil(len_x/2) + vert_pix};
 
-			forward_bound_x = {left_bound_x, right_bound_x};
+			forward_bound = {left_bound, right_bound};
 
 			buffer = (int)ceil(safety_dist/res);
+			printf("footprinted\n");
+			std::cout << left_bound << std::endl;
+			std::cout << right_bound << std::endl;
 		}
-		
-		void checkclear(double** costmap)
+
+		void checkclear()
 		{	
 			bool left_clear=true, right_clear=true, up_clear=true;
 			// right clear
-			for (int i = right_bound_x; i <= right_bound_x + buffer; i++)
+			printf("checking1\n");
+			for (int i = right_bound; i <= right_bound + buffer; i++)
 			{	
 				if (right_clear == false)
 				{
 					break;
 				}
 				for (int j = vert_bound[0] - buffer; j <= vert_bound[1] + buffer; j++)
-				{
-					if (costmap[i][j] > 0)
+				{	
+					if (map_mat[j][i] > 0)
 					{
 						right_clear = false;
 						break;
 					}
 				}
 			}
-
 			// up clear
-			for (int i = left_bound_x - buffer; i <= right_bound_x + buffer; i++)
+			printf("checking2\n");
+			for (int i = left_bound - buffer; i <= right_bound + buffer; i++)
 			{	
 				if (up_clear == false)
 				{
@@ -119,7 +128,7 @@ class Robot
 				}
 				for (int j = vert_bound[1]; j <= vert_bound[1] + buffer; j++)
 				{
-					if (costmap[i][j] > 0)
+					if (map_mat[j][i] > 0)
 					{
 						up_clear = false;
 					}
@@ -127,26 +136,30 @@ class Robot
 			}
 
 			// left clear
-			for (int i = left_bound_x; i >= left_bound_x + buffer; i--)
+			printf("checking3\n");
+			for (int i = left_bound - buffer; i <= left_bound; i++)
 			{	
+				//std::cout << i << std::endl;
 				if (left_clear == false)
 				{
 					break;
 				}
-				for (int j = vert_bound[1]; j <= vert_bound[1] + buffer; j++)
+				for (int j = vert_bound[0]; j <= vert_bound[1] + buffer; j++)
 				{
-					if (costmap[i][j] > 0)
+					if (map_mat[j][i] > 0)
 					{
 						left_clear = false;
 						break;
 					}
 				}
 			}
-			local_planner::CmapClear bools;
-			bools.right = right_clear;
-			bools.left = left_clear;
-			bools.up = up_clear;
-			cmap_clear.publish(bools);
+			printf("checked\n");
+			auto* bl = &bools;
+			bl->right = right_clear;
+			bl->left = left_clear;
+			bl->up = up_clear;
+			cmap_clear.publish(*bl);
+			std::cout << *bl << std::endl;
 			
 		}
 };
