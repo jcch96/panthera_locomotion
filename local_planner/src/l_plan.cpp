e1f2f540fb4f56e9a8f2a16322295fbf1a1e1237
jcch96 <joel_chan@sutd.edu.sg>
2021-03-26 12:15:55 +0800

    fixed matrix

--- local_planner/src/l_plan.cpp
+++ local_planner/src/l_plan.cpp
@@ -27,7 +27,7 @@ class Robot
 		// Footprint info
 		float length, width;
 
-		float safety_dist = 0.5;
+		float safety_dist;
 		int buffer; // number of squares safety distance
 
 		// Map info
@@ -48,6 +48,7 @@ class Robot
 
 			length = nh->getParam("/robot_length", length);
 			width = nh->getParam("/robot_width", width);
+			safety_dist = nh->getParam("/safety_dist", safety_dist);
 		}
 
 		void mapCallback(const nav_msgs::OccupancyGrid& msg)
@@ -58,6 +59,7 @@ class Robot
 			auto data_pts = msg.data;
 			fpCoordinates();
 			int n = 0;
+			std::vector<std::vector<int>> temp_mat;
 			for (int i=0; i<(int)len_x; i++)
 			{	
 				std::vector<int> v = {};
@@ -66,19 +68,19 @@ class Robot
 					v.push_back(data_pts[n]);
 					n++;
 				}
-				map_mat.push_back(v);
-				//std::cout << n << std::endl;
+				temp_mat.push_back(v);
 			}
-			std::cout << "checking clear" << std::endl;
+			//std::cout << temp_mat[0][0] << std::endl;
+			map_mat = temp_mat;
 			checkclear();
 
 		}
 
 		void fpCoordinates()
 		{
-			float centre[1][2];
-			centre[0][0] = len_x/2;
-			centre[0][1] = len_y/2;
+			double centre[2];
+			centre[0] = len_x/2;
+			centre[1] = len_y/2;
 
 			float horz_dist = width/2; // x axis
 			float vert_dist = length/2; // y axis
@@ -86,23 +88,27 @@ class Robot
 			int horz_pix = (int)ceil(horz_dist/res);
 			int vert_pix = (int)ceil(vert_dist/res);
 
-			left_bound = (int)ceil(len_x/2) - vert_pix;
-			right_bound = (int)round(len_x/2) + vert_pix;
-			vert_bound = {(int)round(len_x/2) - vert_pix, (int)ceil(len_x/2) + vert_pix};
+			left_bound = (int)ceil(centre[1]) - vert_pix;
+			right_bound = (int)round(centre[1]) + vert_pix;
+			vert_bound = {(int)round(centre[0]) - vert_pix, (int)ceil(centre[0]) + vert_pix};
 
 			forward_bound = {left_bound, right_bound};
 
 			buffer = (int)ceil(safety_dist/res);
-			printf("footprinted\n");
-			std::cout << left_bound << std::endl;
-			std::cout << right_bound << std::endl;
+			//printf("footprinted\n");
+			/**
+			std::cout << "left: " <<  left_bound << " " << left_bound - buffer << std::endl;
+			std::cout << "right: " << right_bound << " " << right_bound + buffer << std::endl;
+			std::cout << "vert: " << vert_bound[0] << " " << vert_bound[1] << std::endl;
+			std::cout << "centre: " << centre[0] << " " << centre[1] << std::endl;
+			**/
 		}
 
 		void checkclear()
 		{	
 			bool left_clear=true, right_clear=true, up_clear=true;
 			// right clear
-			printf("checking1\n");
+			//printf("checking1\n");
 			for (int i = right_bound; i <= right_bound + buffer; i++)
 			{	
 				if (right_clear == false)
@@ -111,15 +117,18 @@ class Robot
 				}
 				for (int j = vert_bound[0] - buffer; j <= vert_bound[1] + buffer; j++)
 				{	
-					if (map_mat[j][i] > 0)
+					if (map_mat[j][i] == 100)
 					{
 						right_clear = false;
+						//std::cout << map_mat[j][i] << std::endl;
+						//printf("right clear false\n");
 						break;
 					}
+					//std::cout << map_mat[j][i] << std::endl;
 				}
 			}
 			// up clear
-			printf("checking2\n");
+			//printf("checking2\n");
 			for (int i = left_bound - buffer; i <= right_bound + buffer; i++)
 			{	
 				if (up_clear == false)
@@ -128,15 +137,19 @@ class Robot
 				}
 				for (int j = vert_bound[1]; j <= vert_bound[1] + buffer; j++)
 				{
-					if (map_mat[j][i] > 0)
+					if (map_mat[j][i] == 100)
 					{
 						up_clear = false;
+						//std::cout << map_mat[j][i] << std::endl;
+						//printf("up clear false\n");
+						break;
 					}
+					//std::cout << map_mat[j][i] << std::endl;
 				}
 			}
 
 			// left clear
-			printf("checking3\n");
+			//printf("checking3\n");
 			for (int i = left_bound - buffer; i <= left_bound; i++)
 			{	
 				//std::cout << i << std::endl;
@@ -146,14 +159,17 @@ class Robot
 				}
 				for (int j = vert_bound[0]; j <= vert_bound[1] + buffer; j++)
 				{
-					if (map_mat[j][i] > 0)
+					if (map_mat[j][i] == 100)
 					{
 						left_clear = false;
+						//std::cout << map_mat[j][i] << std::endl;
+						//printf("left clear false\n");
 						break;
 					}
+					//std::cout << map_mat[j][i] << std::endl;
 				}
 			}
-			printf("checked\n");
+			//printf("checked\n");
 			auto* bl = &bools;
 			bl->right = right_clear;
 			bl->left = left_clear;
